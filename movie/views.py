287547052022da2fc3e098d3a2597b7d312b18e3
Emmanuel Castañeda Cano <ecastanedc@eafit.edu.co>
2025-07-31 12:07:03 -0500

    Peliculas con estilo

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Movie
 
 def home(request):
     # return HttpResponse("Welcome to the Movie Reviews Home Page") 
     #return render(request, 'home.html') 
-    return render(request, 'home.html', {'name': 'Emmanuel Castañeda Cano'})
+    # return render(request, 'home.html', {'name': 'Emmanuel Castañeda Cano'})
+    searchTerm = request.GET.get('searchMovie')
+    
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    
+    return render(request, 'home.html', {
+        'name': 'Emmanuel Castañeda Cano',
+        'searchTerm': searchTerm,
+        'movies': movies
+    })
 
 def about(request):
     return render(request, 'about.html')
-
