cb77ee8df055f66e98c2938bb443828b33c6794a
Emmanuel Castañeda Cano <ecastanedc@eafit.edu.co>
2025-07-31 11:03:47 -0500

    about page.html

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    # return HttpResponse("Welcome to the Movie Reviews Home Page") 
+    #return render(request, 'home.html') 
+    return render(request, 'home.html', {'name': 'Emmanuel Castañeda Cano'})
 
 def about(request):
     return render(request, 'about.html')
-# Create your views here.
+
